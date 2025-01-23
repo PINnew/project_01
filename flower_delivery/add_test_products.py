69b7ef25aef086a823ae12d656582d0638672e7e
PINnew <pinnew@ya.ru>
2025-01-23 18:09:16 +0400

    add cart_navbar

--- flower_delivery/add_test_products.py
+++ flower_delivery/add_test_products.py
@@ -9,9 +9,9 @@ from products.models import Product
 
 # Словарь с тестовыми данными
 test_products = [
-    {"name": "Роза красная", "price": 150, "product_type": "rose", "color": "red", "image": "product_images/rose_red.jpg"},
-    {"name": "Роза белая", "price": 140, "product_type": "rose", "color": "white", "image": "product_images/rose_white.jpg"},
-    {"name": "Тюльпан жёлтый", "price": 120, "product_type": "tulip", "color": "yellow", "image": "product_images/tulip_yellow.jpg"},
+    {"name": "Роза красная", "price": 100, "product_type": "rose", "color": "red", "image": "product_images/rose_r.jpg"},
+    {"name": "Роза белая", "price": 100, "product_type": "rose", "color": "white", "image": "product_images/rose_white.jpg"},
+    {"name": "Тюльпан жёлтый", "price": 80, "product_type": "tulip", "color": "yellow", "image": "product_images/tulip_yellow.jpg"},
     {"name": "Хризантема розовая", "price": 130, "product_type": "chrysanthemum", "color": "pink", "image": "product_images/chrysanthemum_pink.jpg"},
 ]
 
